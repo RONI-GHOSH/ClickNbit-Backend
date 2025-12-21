0f64fea234a1e3f94fa285765ba5bf895a76ceff
Roni Ghosh <141951636+RONI-GHOSH@users.noreply.github.com>
2025-12-21 16:53:32 +0530

    move to valkeyUrl (testing only)

--- config/valkey.js
+++ config/valkey.js
@@ -5,17 +5,19 @@ let redis;
 
 function getRedis() {
   if (!redis) {
-    redis = new Redis({
-      host: process.env.VALKEY_HOST,
-      port: Number(process.env.VALKEY_PORT || 6379),
-      password: process.env.VALKEY_PASSWORD,
+    const serviceUrl=process.env.VALKEY_SERVICE_URL;
+    redis = new Redis(serviceUrl);
+    // redis = new Redis({
+    //   host: process.env.VALKEY_HOST,
+    //   port: Number(process.env.VALKEY_PORT || 6379),
+    //   password: process.env.VALKEY_PASSWORD,
 
-      lazyConnect: true,
-      enableOfflineQueue: false,
-      maxRetriesPerRequest: 1,
+    //   lazyConnect: true,
+    //   enableOfflineQueue: false,
+    //   maxRetriesPerRequest: 1,
 
-      retryStrategy: () => null, // important for Lambda
-    });
+    //   retryStrategy: () => null, // important for Lambda
+    // });
 
     redis.on("connect", () => console.log("✅ Valkey connected"));
     redis.on("close", () => console.warn("⚠️ Valkey connection closed"));
