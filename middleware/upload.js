88ed8f9a758439ca17266d1839c264de8ec8be81
Roni Ghosh <141951636+RONI-GHOSH@users.noreply.github.com>
2026-09-25 16:59:19 +0530

    fix video size params

--- middleware/upload.js
+++ middleware/upload.js
@@ -21,7 +21,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 20 * 1024 * 1024 },
+  limits: { fileSize: 200 * 1024 * 1024 },
 });
 
 
